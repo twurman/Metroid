66f2b21a44b4afe10e267f74546655501486bbec
Adam Schnitzer <adamschnitzer@gmail.com>
2015-01-19 14:47:28 -0500

    Add basic Samus movement

---
+++ Assets/Scripts/SamusMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SamusMovement : MonoBehaviour {
+
+	public float SpeedMultiplier = 10.0f;
+
+	void FixedUpdate() {
+		float hSpeed = Input.GetAxis("Horizontal");
+
+		if (hSpeed > 0) {
+			transform.localScale = new Vector3( 1, 1, 1);
+		} else if (hSpeed < 0) {
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+
+		this.rigidbody2D.velocity = new Vector2 (hSpeed * SpeedMultiplier, this.rigidbody2D.velocity.y);
+	}
+}
