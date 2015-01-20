af59e5f74634fe49b542dfe3ead2892c057271f6
Adam Schnitzer <adamschnitzer@gmail.com>
2015-01-20 18:41:30 -0500

    add basic shooting

--- Assets/Scripts/SamusMovement.cs
+++ Assets/Scripts/SamusMovement.cs
@@ -9,6 +9,18 @@ public class SamusMovement : MonoBehaviour
 		public bool falling = false;
 		public float groundHeight = 0;
 
+		public float fireRate = 0.5F;
+		private float nextFire = 0.0F; 
+		public GameObject projectile;
+
+		void Update() {
+			if (Input.GetButton("Fire") && Time.time > nextFire) {
+				nextFire = Time.time + fireRate;
+				GameObject clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+				clone.rigidbody2D.velocity = new Vector2(transform.localScale.x, 0);
+			}
+		}
+
 		void FixedUpdate ()
 		{
 				float hSpeed = Input.GetAxis ("Horizontal");
