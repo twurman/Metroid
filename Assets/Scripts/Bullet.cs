4615baaf472ac20419516a0fd00e3a1d9e30e197
Adam Schnitzer <adamschnitzer@gmail.com>
2015-01-20 18:49:59 -0500

    really add gun stuff

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public float velocity = 1.0f;
+	public float maxDistance = 3.0f;
+
+	private float initialPosition;
+
+	// Use this for initialization
+	void Start () {
+		this.rigidbody2D.velocity = new Vector2 (velocity*this.rigidbody2D.velocity.x, 0);
+		initialPosition = this.rigidbody2D.position.x;
+	}
+
+	void FixedUpdate() {
+		if (Mathf.Abs(this.rigidbody2D.position.x - initialPosition) > maxDistance) {
+			Destroy(this.gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
