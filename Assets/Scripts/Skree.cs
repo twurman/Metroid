1e0213c392087cca8d74617f267977a899b533d8
Adam Schnitzer <adamschnitzer@gmail.com>
2015-01-20 22:55:43 -0500

    add skree

---
+++ Assets/Scripts/Skree.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skree : PE_Obj {
+
+	public GameObject player;
+
+	public float AttackDistance = 5;
+
+	private bool collided = false;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Mathf.Abs(player.transform.position.x - this.gameObject.transform.position.x) < AttackDistance) {
+			this.grav = PE_GravType.constant;
+		}
+	}
+
+	void LateUpdate() {
+		if (collided) {
+			Destroy (this.gameObject);
+			PhysEngine.objs.Remove(this);
+		}
+
+	}
+
+
+
+	void OnTriggerEnter(Collider other) {
+		collided = true;
+	}
+}
