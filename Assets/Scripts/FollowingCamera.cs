c03f024744e0d89887c87faf5611d6831eee7b6a
Adam Schnitzer <adamschnitzer@gmail.com>
2015-01-20 21:42:41 -0500

    add following cam

---
+++ Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowingCamera : MonoBehaviour {
+	
+	public GameObject camTarget;
+	
+	public float xOffset = 0;
+	public float yOffset = 0;
+	
+	void LateUpdate() {
+		Debug.Log (this.camTarget.transform.position);
+		this.transform.position = new Vector3(camTarget.transform.position.x + xOffset,
+		                                      camTarget.transform.position.y + yOffset,
+		                                      this.transform.position.z);
+	}
+}
