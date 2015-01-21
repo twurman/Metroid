969d8e24b785955b60141cd106b0ee8ef721a41d
Tim W <twurman@umich.edu>
2015-01-20 21:53:43 -0500

    camera follow

--- Assets/Scripts/FollowingCamera.cs
+++ Assets/Scripts/FollowingCamera.cs
@@ -2,16 +2,22 @@
 using System.Collections;
 
 public class FollowingCamera : MonoBehaviour {
+
+	//from http://answers.unity3d.com/questions/29183/2d-camera-smooth-follow.html
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
 	
-	public GameObject camTarget;
-	
-	public float xOffset = 0;
-	public float yOffset = 0;
-	
-	void LateUpdate() {
-		Debug.Log (this.camTarget.transform.position);
-		this.transform.position = new Vector3(camTarget.transform.position.x + xOffset,
-		                                      camTarget.transform.position.y + yOffset,
-		                                      this.transform.position.z);
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target)
+		{
+			Vector3 point = camera.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, point.y, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+		
 	}
 }
