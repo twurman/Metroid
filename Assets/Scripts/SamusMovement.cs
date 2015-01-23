e372821b18faa0739e6a35a79399964881a4f726
Adam Schnitzer <adamschnitzer@gmail.com>
2015-01-23 13:21:29 -0500

    make bullet shoot from gun

--- Assets/Scripts/SamusMovement.cs
+++ Assets/Scripts/SamusMovement.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class SamusMovement : MonoBehaviour
 {
-
+		public float BulletOffset_x = 0f;
+		public float BulletOffset_y = 0f;
+		
 		public float fireRate = 0.5F;
 		private float nextFire = 0.0F; 
 		public GameObject projectile;
@@ -11,7 +13,12 @@ public class SamusMovement : MonoBehaviour
 		void Update() {
 			if (Input.GetButton("Fire") && Time.time > nextFire) {
 				nextFire = Time.time + fireRate;
-				GameObject clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+
+				Vector3 bullet_pos = new Vector3(
+											transform.position.x + (BulletOffset_x * transform.localScale.x),
+			                                transform.position.y + BulletOffset_y,
+											transform.position.z);
+				GameObject clone = Instantiate(projectile, bullet_pos, transform.rotation) as GameObject;
 				clone.rigidbody2D.velocity = new Vector2(transform.localScale.x, 0);
 			}
 		}
