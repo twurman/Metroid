c12558abffee39976c16dd35c38a505213333af3
Tim W <twurman@umich.edu>
2015-01-25 21:38:53 -0500

    updated physics engine & prefabs

--- Assets/Scripts/PE_Obj.cs
+++ Assets/Scripts/PE_Obj.cs
@@ -1,51 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+//using UnityEditor;
 
 
 
 public class PE_Obj : MonoBehaviour {
 	public bool			still = false;
-    public bool         has_mass = true;
-
-	public PE_Collider	coll = PE_Collider.aabb;
+	public PE_Collider	coll = PE_Collider.sphere;
 	public PE_GravType	grav = PE_GravType.constant;
 	
 	public Vector3		acc = Vector3.zero;
-	
+
 	public Vector3		vel = Vector3.zero;
 	public Vector3		vel0 = Vector3.zero;
-	
-	public Vector3		pos0 = Vector3.zero;
-	public Vector3		pos1 = Vector3.zero;
-	
+	public Vector3		velRel = Vector3.zero; // Velocity based on relative position from last frame to this
+
+	public Vector3		_pos0 = Vector3.zero;
+	public Vector3		_pos1 = Vector3.zero;
+
 	public PE_Dir		dir = PE_Dir.still;
-	
+
 	public PE_Obj		ground = null; // Stores whether this is on the ground
-	
-	
-	void Start() {
-		if (PhysEngine.objs.IndexOf(this) == -1) {
-			PhysEngine.objs.Add(this);
+
+	public Vector3		pos0 {
+		get { return( _pos0); }
+		set {
+			float d = (value - _pos0).magnitude;
+			if (d > 1 && gameObject.name == "PC") {
+				Debug.Log ("Big change in pos0!");
+			}
+			_pos0 = value;
 		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-
+	public Vector3		pos1 {
+		get { return( _pos1); }
+		set {
+			float d = (value - _pos1).magnitude;
+			if (d > 1 && gameObject.name == "PC") {
+				Debug.Log ("Big change in pos1!");
+			}
+			_pos1 = value;
+		}
 	}
 
-    bool IgnoreCollision(Collider other) {
+	void Start() {
+		if (PhysEngine.objs.IndexOf(this) == -1) {
+			_pos1 = _pos0 = transform.position;
+			PhysEngine.objs.Add(this);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	bool IgnoreCollision(Collider other) {
 		bool ignore = (other.GetComponent<Enemy> () != null && gameObject.layer == LayerMask.NameToLayer ("Player Bullet")) ||
-						(this.GetComponent<Enemy> () != null && other.gameObject.layer == LayerMask.NameToLayer ("Player Bullet"));
-        return ignore;
-    }
+			(this.GetComponent<Enemy> () != null && other.gameObject.layer == LayerMask.NameToLayer ("Player Bullet"));
+		return ignore;
+	}
 	
 	void OnTriggerEnter(Collider other) {
 		// Ignore collisions of still objects
 		if (still) return;
 		
 		PE_Obj otherPEO = other.GetComponent<PE_Obj>();
-		if (otherPEO == null || ignoreCollision(other)) return;
+		if (otherPEO == null || IgnoreCollision(other)) return;
 		
 		ResolveCollisionWith(otherPEO);
 	}
@@ -59,7 +82,7 @@ public class PE_Obj : MonoBehaviour {
 		if (still) return;
 		
 		PE_Obj otherPEO = other.GetComponent<PE_Obj>();
-		if (otherPEO == null || ignoreCollision(other)) return;
+		if (otherPEO == null || IgnoreCollision(other)) return;
 		
 		// This sets ground to null if we fall off of the current ground
 		// Jumping will also set ground to null
@@ -67,14 +90,31 @@ public class PE_Obj : MonoBehaviour {
 			ground = null;
 		}
 	}
-	
+
+	void OnDrawGizmos() {
+		if (vel.magnitude != 0) {
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireSphere(a0,0.2f);
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawLine(a0,a1);
+			Gizmos.DrawWireSphere(a1,0.2f);
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine(a1,posFinal);
+			Gizmos.DrawWireSphere(posFinal,0.2f);
+		}
+	}
+
+	public Vector3 a0, a1, b, delta, pU, posFinal; // a0-moving corner last frame, a1-moving corner now, b-comparison corner on other object
+
 	void ResolveCollisionWith(PE_Obj that) {
+
 		// Assumes that "that" is still
-		Vector3 posFinal = pos1; // Sets a defaut value for posFinal
-		
+//		Vector3 posFinal;
+		posFinal = pos1; // Sets a defaut value for posFinal
+
 		switch (this.coll) {
 		case PE_Collider.sphere:
-			
+
 			switch (that.coll) {
 			case PE_Collider.sphere:
 				// Sphere / Sphere collision
@@ -83,138 +123,144 @@ public class PE_Obj : MonoBehaviour {
 				thisR = Mathf.Max( this.transform.lossyScale.x, this.transform.lossyScale.y, this.transform.lossyScale.z ) / 2;
 				thatR = Mathf.Max( that.transform.lossyScale.x, that.transform.lossyScale.y, that.transform.lossyScale.z ) / 2;
 				rad = thisR + thatR;
-				
+
 				Vector3 delta = pos1 - that.transform.position;
 				delta.Normalize();
 				delta *= rad;
-				
+
 				posFinal = that.transform.position + delta;
 				break;
 			}
-			
+
 			break;
-			
+
 		case PE_Collider.aabb:
-			
+
 			switch (that.coll) {
 			case PE_Collider.aabb:
 				// AABB / AABB collision
 				// Axis-Aligned Bounding Box
 				// With AABB collisions, we're usually concerned with corners and deciding which corner to consider when making comparisons.
 				// I believe that this corner should be determined by looking at the velocity of the moving body (this one)
-				
-				Vector3 a0, a1, b; // a0-moving corner last frame, a1-moving corner now, b-comparison corner on other object
+
+//				Vector3 a0, a1, b, delta, pU; // a0-moving corner last frame, a1-moving corner now, b-comparison corner on other object
 				a0 = a1 = b = Vector3.zero;	 // Sets a default value to keep the compiler from complaining
-				Vector3 delta = pos1 - pos0;
-				
+				delta = pos1 - pos0;
+
 				if (dir == PE_Dir.down) {
-					// Just resolve to be on top
+					// If a0 was above b and a1 is below b resolve to be on top
 					a1 = pos1;
-					a1.y -= transform.lossyScale.y * transform.collider.bounds.size.y /2f;
+					a1.y -= transform.lossyScale.y * transform.collider.bounds.size.y/2f;
+					a0 = a1 - delta;
 					b = that.pos1;
-					b.y += that.transform.lossyScale.y /2f;
-					if (b.y > a1.y) {
+					b.y += that.transform.lossyScale.y/2f;
+					if ( PhysEngine.GEQ( a0.y, b.y ) && b.y > a1.y) {
 						posFinal.y += Mathf.Abs( a1.y - b.y );
-					}
-					// Handle vel
-					vel.y = 0;
-					if (ground == null) ground = that;
-					break; // Exit this switch statement: switch (that.coll)
-				}
-				
-				if (dir == PE_Dir.up) {
-					// Just resolve to be below
-					a1 = pos1;
-					a1.y += transform.lossyScale.y * transform.collider.bounds.size.y /2f;
-					b = that.pos1;
-					b.y -= that.transform.lossyScale.y /2f;
-					if (b.y < a1.y) {
-						posFinal.y -= Mathf.Abs( a1.y - b.y );
-					}
-					// Handle vel
-					vel.y = 0;
-					break; // Exit this switch statement: switch (that.coll)
-				}
-				
-				if (dir == PE_Dir.upRight) { // Bottom, Left is the comparison corner
-					a1 = pos1;
-					a1.x += transform.lossyScale.x * transform.collider.bounds.size.x /2f;
-					a1.y += transform.lossyScale.y * transform.collider.bounds.size.y /2f;
-					a0 = a1 - delta;
-					b = that.pos1;
-					b.x -= that.transform.lossyScale.x /2f;
-					b.y -= that.transform.localScale.y /2f;
-//					print ("collision up right");
-				}
-				
-				if (dir == PE_Dir.upLeft) { // Bottom, Right is the comparison corner
-					a1 = pos1;
-					a1.x -= transform.lossyScale.x * transform.collider.bounds.size.x /2f;
-					a1.y += transform.lossyScale.y * transform.collider.bounds.size.y /2f;
-					a0 = a1 - delta;
-					b = that.pos1;
-					b.x += that.transform.lossyScale.x /2f;
-					b.y -= that.transform.localScale.y /2f;
-//					print ("collision up left");
-				}
-				
-				if (dir == PE_Dir.downLeft) { // Top, Right is the comparison corner
-					a1 = pos1;
-					a1.x -= transform.lossyScale.x * transform.collider.bounds.size.x  /2f;
-					a1.y -= transform.lossyScale.y * transform.collider.bounds.size.y /2f;
-					a0 = a1 - delta;
-					b = that.pos1;
-					b.x += that.transform.lossyScale.x /2f;
-					b.y += that.transform.localScale.y /2f;
-//					print ("collision down left");
-				}
-				
-				if (dir == PE_Dir.downRight) { // Top, Left is the comparison corner
-					a1 = pos1;
-					a1.x += transform.lossyScale.x * transform.collider.bounds.size.x /2f;
-					a1.y -= transform.lossyScale.y * transform.collider.bounds.size.y /2f;
-					a0 = a1 - delta;
-					b = that.pos1;
-					b.x -= that.transform.lossyScale.x /2f;
-					b.y += that.transform.localScale.y /2f;
-//					print ("collision down right");
-				}
-				
-				// In the x dimension, find how far along the line segment between a0 and a1 we need to go to encounter b
-				float u = (b.x - a0.x) / (a1.x - a0.x);
-				
-				// Determine this point using linear interpolation (see the appendix of the book)
-				Vector3 pU = (1-u)*a0 + u*a1;
-				
-				// Use pU.y vs. b.y to tell which side of PE_Obj "that" PE_Obj "this" should be on
-				switch (dir) {
-				case PE_Dir.upRight:
-					if (a0.x < b.x && a0.y > b.y) { // hit the left side
-						posFinal.x -= Mathf.Abs(a1.x - b.x);
-						
-						// Handle vel
-						vel.x = 0;
-						
-					} else { // hit the bottom
-						posFinal.y -= Mathf.Abs(a1.y - b.y);
-						
 						// Handle vel
 						vel.y = 0;
 						
+						if (ground == null) ground = that;
+					}
+					break; // Exit this switch statement: switch (that.coll)
+				}
+
+				if (dir == PE_Dir.up) {
+					// If a0 was below b and a1 is above b resolve to be below
+					a1 = pos1;
+					a1.y += transform.lossyScale.y * transform.collider.bounds.size.y/2f;
+					a0 = a1 - delta;
+					b = that.pos1;
+					b.y -= that.transform.lossyScale.y/2f;
+					if ( PhysEngine.LEQ( a0.y, b.y ) && b.y < a1.y) {
+						posFinal.y -= Mathf.Abs( a1.y - b.y );
+						// Handle vel
+						vel.y = 0;
+						
+						break; // Exit this switch statement: switch (that.coll)
+					}
+				}
+
+				if (dir == PE_Dir.upRight) { // Bottom, Left is the comparison corner
+					a1 = pos1;
+					a1.x += transform.lossyScale.x * transform.collider.bounds.size.x/2f;
+					a1.y += transform.lossyScale.y * transform.collider.bounds.size.y/2f;
+					a0 = a1 - delta;
+					b = that.pos1;
+					b.x -= that.transform.lossyScale.x/2f;
+					b.y -= that.transform.lossyScale.y/2f;
+				}
+
+				if (dir == PE_Dir.upLeft) { // Bottom, Right is the comparison corner
+					a1 = pos1;
+					a1.x -= transform.lossyScale.x * transform.collider.bounds.size.x/2f;
+					a1.y += transform.lossyScale.y* transform.collider.bounds.size.y/2f;
+					a0 = a1 - delta;
+					b = that.pos1;
+					b.x += that.transform.lossyScale.x/2f;
+					b.y -= that.transform.lossyScale.y/2f;
+				}
+
+				if (dir == PE_Dir.downLeft) { // Top, Right is the comparison corner
+ 					a1 = pos1;
+					a1.x -= transform.lossyScale.x * transform.collider.bounds.size.x/2f;
+					a1.y -= transform.lossyScale.y * transform.collider.bounds.size.y/2f;
+					a0 = a1 - delta;
+					b = that.pos1;
+					b.x += that.transform.lossyScale.x/2f;
+					b.y += that.transform.lossyScale.y/2f;
+				}
+
+				if (dir == PE_Dir.downRight) { // Top, Left is the comparison corner
+					a1 = pos1;
+					a1.x += transform.lossyScale.x * transform.collider.bounds.size.x/2f;
+					a1.y -= transform.lossyScale.y * transform.collider.bounds.size.y/2f;
+					a0 = a1 - delta;
+					b = that.pos1;
+					b.x -= that.transform.lossyScale.x/2f;
+					b.y += that.transform.lossyScale.y/2f;
+				}
+
+				// In the x dimension, find how far along the line segment between a0 and a1 we need to go to encounter b
+				float u = (b.x - a0.x) / (a1.x - a0.x);
+
+				// Determine this point using linear interpolation (see the appendix of the book)
+				pU = (1-u)*a0 + u*a1;
+
+				// Find distance we would have to offset in x or y
+				float offsetX = Mathf.Abs(a1.x - b.x);
+				float offsetY = Mathf.Abs(a1.y - b.y);
+				bool usedX = false;
+
+				// Use pU.y vs. b.y to tell which side of PE_Obj "that" PE_Obj "this" should be on
+				switch (dir) {
+				case PE_Dir.upRight:
+					if (pU.y > b.y || u == 0) { // hit the left side
+						posFinal.x -= offsetX;
+
+						// Handle vel
+						vel.x = 0;
+
+						usedX = true;
+					} else { // hit the bottom
+						posFinal.y -= offsetY;
+						
+						// Handle vel
+						vel.y = 0;
+
 					}
 					break;
-					
+
 				case PE_Dir.downRight:
-//					print("pU.y = " + pU.y + "  b.y = " + b.y);
-					if (pU.y < b.y) { // hit the left side
-						posFinal.x -= Mathf.Abs(a1.x - b.x);
+					if (pU.y < b.y || u == 0) { // hit the left side
+						posFinal.x -= offsetX;
 						
 						// Handle vel
 						vel.x = 0;
 						
+						usedX = true;
 					} else { // hit the top
-						posFinal.y += Mathf.Abs(a1.y - b.y);
-						
+						posFinal.y += offsetY;
+
 						// Handle vel
 						vel.y = 0;
 						
@@ -223,30 +269,32 @@ public class PE_Obj : MonoBehaviour {
 					break;
 					
 				case PE_Dir.upLeft:
-					if (pU.y > b.y) { // hit the right side
-						posFinal.x += Mathf.Abs(a1.x - b.x);
+					if (pU.y > b.y || u == 0) { // hit the right side
+						posFinal.x += offsetX;
 						
 						// Handle vel
 						vel.x = 0;
 						
+						usedX = true;
 					} else { // hit the bottom
-						posFinal.y -= Mathf.Abs(a1.y - b.y);
+						posFinal.y -= offsetY;
 						
 						// Handle vel
 						vel.y = 0;
-						
+
 					}
 					break;
 					
 				case PE_Dir.downLeft:
-					if (pU.y < b.y) { // hit the right side
-						posFinal.x += Mathf.Abs(a1.x - b.x);
+					if (pU.y < b.y || u == 0) { // hit the right side
+						posFinal.x += offsetX;
 						
 						// Handle vel
 						vel.x = 0;
 						
+						usedX = true;
 					} else { // hit the top
-						posFinal.y += Mathf.Abs(b.y - a1.y);
+						posFinal.y += offsetY;
 						
 						// Handle vel
 						vel.y = 0;
@@ -255,15 +303,23 @@ public class PE_Obj : MonoBehaviour {
 					}
 					break;
 				}
+
 				
+//				if ( (pos1-posFinal).magnitude>1f || (usedX && offsetX > 1) || (!usedX && offsetY > 1) ) {
+//					print ("offX:\t"+offsetX+"\toffY:\t"+offsetY+"\npF:\t"+posFinal+"\na0:\t"+a0+"\na1:\t"+a1+"\n b:\t"+b+"\n u:\t"+u+"\npU:\t"+pU);
+//					EditorApplication.isPaused = true;
+//				} else {
+//					print ((pos1-posFinal).magnitude);
+//				}
+
 				break;
 			}
-			
+
 			break;
 		}
-		
+
 		transform.position = pos1 = posFinal;
 	}
-	
-	
+
+
 }
--- Assets/Scripts/PhysEngine.cs
+++ Assets/Scripts/PhysEngine.cs
@@ -17,8 +17,6 @@ public enum PE_Dir { // The direction in which the PE_Obj is moving
 	still,
 	up,
 	down,
-	right,
-	left,
 	upRight,
 	downRight,
 	downLeft,
@@ -27,40 +25,45 @@ public enum PE_Dir { // The direction in which the PE_Obj is moving
 
 public class PhysEngine : MonoBehaviour {
 	static public List<PE_Obj>	objs;
-	
+	static public float			closeEnough = 0.1f;
+
 	public Vector3		gravity = new Vector3(0,-9.8f,0);
-	
+
 	// Use this for initialization
 	void Awake() {
 		objs = new List<PE_Obj>();
 	}
-	
-	
+
+
 	void FixedUpdate () {
 		// Handle the timestep for each object
 		float dt = Time.fixedDeltaTime;
 		foreach (PE_Obj po in objs) {
 			TimeStep(po, dt);
 		}
-		
+
 		// Resolve collisions
-		
-		
+		// In a normal physics engine, this is where collisions would be resolved.
+		// But in Unity, because of the way we're using triggers to sense collisions, this step happens in OnCollisionEnter() and OnCollisionStay()
+
+
 		// Finalize positions
 		foreach (PE_Obj po in objs) {
 			po.transform.position = po.pos1;
 		}
-		
+
 	}
-	
-	
+
+
 	public void TimeStep(PE_Obj po, float dt) {
 		if (po.still) {
 			po.pos0 = po.pos1 = po.transform.position;
 			return;
 		}
-		
+
 		// Velocity
+		po.velRel = (po.pos1 - po.pos0) / dt;
+
 		po.vel0 = po.vel;
 		Vector3 tAcc = po.acc;
 		switch (po.grav) {
@@ -69,7 +72,7 @@ public class PhysEngine : MonoBehaviour {
 			break;
 		}
 		po.vel += tAcc * dt;
-		
+
 		if (po.vel.x==0) { // Special case when po.vel.x == 0
 			if (po.vel.y > 0) {
 				po.dir = PE_Dir.up;
@@ -78,17 +81,42 @@ public class PhysEngine : MonoBehaviour {
 			}
 		} else if (po.vel.x>0 && po.vel.y>0) {
 			po.dir = PE_Dir.upRight;
-		} else if (po.vel.x>0 && po.vel.y<0) {
+		} else if (po.vel.x>0 && po.vel.y<=0) {
 			po.dir = PE_Dir.downRight;
-		} else if (po.vel.x<0 && po.vel.y<0) {
+		} else if (po.vel.x<0 && po.vel.y<=0) {
 			po.dir = PE_Dir.downLeft;
 		} else if (po.vel.x<0 && po.vel.y>0) {
 			po.dir = PE_Dir.upLeft;
 		}
-		
+
 		// Position
-		po.pos1 = po.pos0 = po.transform.position;
+		po.pos0 = po.transform.position;
 		po.pos1 += po.vel * dt;
-		
+
 	}
+
+
+	// Static equality functions to deal with floating point math errors
+	static public bool EQ(float f0, float f1) {
+		if ( Mathf.Abs(f1-f0) <= closeEnough ) {
+			return( true );
+		}
+		return( false );
+	}
+
+	static public bool LEQ(float f0, float f1) {
+		if ( f0 < f1 || Mathf.Abs(f1-f0) <= closeEnough ) {
+			return( true );
+		}
+		return( false );
+	}
+
+	static public bool GEQ(float f0, float f1) {
+		if ( f0 > f1 || Mathf.Abs(f1-f0) <= closeEnough ) {
+			return( true );
+		}
+		return( false );
+	}
+	
+
 }
