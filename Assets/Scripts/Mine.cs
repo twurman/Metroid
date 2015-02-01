624ece58acd8a20ae424c4da4f0f8b022c74711f
Tim W <twurman@umich.edu>
2015-02-01 18:05:52 -0500

    added mine layer and updated damage to enemies

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -5,9 +5,12 @@ public class Mine : MonoBehaviour {
 
 	public Sprite normal, exploded;
 	private float delay = 2f;
+	private float destroyDelay = 3f;
 	private float explodeTime;
 	private bool exploding = false;
 	private float damage = 10f;
+	private bool dealtDamage = false;
+	private float radius = 3f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,17 +20,22 @@ public class Mine : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(exploding){
-			if(Time.time >= explodeTime + delay){
+			if(Time.time >= explodeTime + destroyDelay) {
+				print ("destroy");
+				Destroy(this.gameObject);
+			} else if(Time.time >= explodeTime + delay){
+				GetComponent<SpriteRenderer>().color = Color.white;
 				GetComponent<SpriteRenderer>().sprite = exploded;
-				foreach(Collider other in Physics.OverlapSphere(transform.position, 3f)){
-					if(other.gameObject.layer == LayerMask.NameToLayer("Player")){
+				foreach(Collider other in Physics.OverlapSphere(transform.position, radius)){
+					if(!dealtDamage && other.gameObject.layer == LayerMask.NameToLayer("Player")){
 						other.GetComponent<SamusMovement>().CauseDamage(damage);
-						print ("Player in range");
+						dealtDamage = true;
 					} if(other.gameObject.layer == LayerMask.NameToLayer("Enemy")){
-						other.GetComponent<Enemy>().CauseDamage(damage);
-						print("enemy hit");
+						other.GetComponent<Enemy>().CauseDamage(damage * 100000);
 					}
 				}
+			} else {
+				GetComponent<SpriteRenderer>().color = Color.red;
 			}
 		}
 	}
