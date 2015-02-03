ce82a36f5279cf542a712347d4c49b892971758a
Tim W <twurman@umich.edu>
2015-02-02 20:22:58 -0500

    added start and finish screens

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.layer == LayerMask.NameToLayer ("Player")) {
+			Application.LoadLevel(3);
+		}
+	}
+}
---
+++ Assets/Scripts/Start_Script.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Z)){
+			Application.LoadLevel(1);
+		}
+		if(Input.GetKeyDown(KeyCode.X)){
+			Application.LoadLevel(2);
+		}
+	}
+}
