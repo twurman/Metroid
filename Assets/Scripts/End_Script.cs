88c888b9f55e77ee653ca2e392e56f9b52bc772e
Tim W <twurman@umich.edu>
2015-02-02 20:36:54 -0500

    start and end scenes implemented

--- Assets/Scripts/End_Script.cs
+++ Assets/Scripts/End_Script.cs
@@ -10,6 +10,8 @@ public class End_Script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.R)){
+			Application.LoadLevel(0);
+		}
 	}
 }
