84421c8b3b3be0a405c8a42a5e37ba77c7981b97
Tim W <twurman@umich.edu>
2015-02-02 21:50:55 -0500

    breakable wall destroy bullets

---
+++ Assets/Scripts/BreakableWall.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableWall : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.layer == LayerMask.NameToLayer ("Player Bullet")) {
+			PhysEngine.objs.Remove(other.GetComponent<PE_Obj>());
+			Destroy(other.gameObject);
+		}
+	}
+}
