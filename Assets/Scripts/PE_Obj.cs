5699cd775f1846dd7af63b1d4221c15acb3cdc37
Tim W <twurman@umich.edu>
2015-02-05 19:16:57 -0500

    updated prefabs

--- Assets/Scripts/PE_Obj.cs
+++ Assets/Scripts/PE_Obj.cs
@@ -58,7 +58,7 @@ public class PE_Obj : MonoBehaviour {
 
 	void LateUpdate(){
 		if(gameObject.layer == LayerMask.NameToLayer("Player")){
-			if(Mathf.Abs((posFinal - pos0).magnitude) - Mathf.Abs(2 * vel0.magnitude * Time.deltaTime) >= 20f){
+			if(Mathf.Abs((posFinal - pos0).magnitude) - Mathf.Abs(vel0.magnitude * Time.deltaTime) >= 10f){
 				transform.position = pos0;
 				posFinal = pos1 = pos0;
 				vel = Vector3.zero;
